b401968961c63eddc9604a96704a80ad8436b01e
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2019-02-21 01:52:04 +0100

    test: start writing test

--- test/test.js
+++ test/test.js
@@ -1,6 +1,69 @@
 // Require Third-party Dependencies
 const avaTest = require("ava");
+const is = require("@slimio/is");
 
-avaTest("Husky passed Test", (assert) => {
-    assert.pass();
+// Require Internal Dependencies
+const TimeMap = require("../");
+
+avaTest("Exported data", (assert) => {
+    assert.true(is.classObject(TimeMap));
+    assert.true(is.number(TimeMap.DEFAULT_TIMELIFE_MS));
+});
+
+avaTest("new TimeMap(null) must throw TypeError", (assert) => {
+    assert.throws(() => {
+        new TimeMap(null);
+    }, { instanceOf: TypeError, message: "timeLifeMs must be a number" });
+});
+
+avaTest("construct new TimeMap", (assert) => {
+    const map = new TimeMap();
+    assert.is(map.timeLife, TimeMap.DEFAULT_TIMELIFE_MS);
+    assert.is(Object.keys(map).length, 3);
+    assert.is(Reflect.ownKeys(map).length, 6);
+
+    const mapCusto = new TimeMap(4000);
+    assert.is(mapCusto.timeLife, 4000);
+});
+
+avaTest("set key must be string or symbol", (assert) => {
+    assert.throws(() => {
+        const map = new TimeMap();
+        map.set(null, null);
+    }, { instanceOf: TypeError, message: "key must be a string or a symbol" });
+});
+
+avaTest("set key in TimeMap", async(assert) => {
+    assert.plan(4);
+    const map = new TimeMap(100);
+    const symFoo = Symbol("foo");
+    map.on("expiration", () => {
+        assert.pass();
+    });
+
+    const ret = map.set(symFoo, "bar");
+    assert.is(ret, void 0);
+    assert.true(map.has(symFoo));
+
+    await new Promise((resolve) => setTimeout(resolve, 110));
+    assert.false(map.has(symFoo));
+});
+
+avaTest("get key in TimeMap", async(assert) => {
+    assert.plan(4);
+    const map = new TimeMap(100);
+    map.on("expiration", () => {
+        assert.pass();
+    });
+
+    map.set("foo", "bar");
+    map.set("woo", "moo");
+
+    let ret = map.get("foo");
+    assert.is(ret, "bar");
+
+    ret = map.get("world!");
+    assert.is(ret, null);
+
+    await new Promise((resolve) => setTimeout(resolve, 110));
 });
